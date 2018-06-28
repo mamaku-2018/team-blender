6a0c9db42ee55b438a305a8f670e9bc3a3bb3586
Rachael Peri <raheraperi@Rachaels-MacBook-Air.local>
2018-06-28 18:37:29 +1200

    radio buttons component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Radiobuttons from './Radiobuttons'
 
 class App extends React.Component {
   constructor (props) {
@@ -12,7 +13,7 @@ class App extends React.Component {
     return (
       <div className='app'>
         <h1>Fullstack Boilerplate</h1>
-
+        <Radiobuttons />
       </div>
     )
   }
