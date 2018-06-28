768c9d56068bd6ec222d64c2d9c7a828f7c5403b
sam-by-name <sam-sanders@hotmail.co.uk>
2018-06-28 19:22:44 +1200

    route and component for header made

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
+import Header from './Header'
 import RadioButtons from './Radiobuttons'
 import Input from './Input'
 import DropDown from './Dropdown'
@@ -18,10 +19,11 @@ class App extends React.Component {
     return (
       <Router>
         <div className='app'>
-          <Route path='/home' component={Pool} />
+          <Route path='/home' component={Header} />
           <Route path='/home' component={RadioButtons} />
           <Route path='/home' component={Input} />
           <Route path='/home' component={DropDown} />
+          <Route path='/home' component={Pool} />
           <Route path='/home' component={Results} />
         </div>
       </Router>
