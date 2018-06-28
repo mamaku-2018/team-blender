f893232a1d2fcad76019efa4ffcb24e39372cc9d
Rachael Peri <raheraperi@Rachaels-MacBook-Air.local>
2018-06-28 19:02:46 +1200

    Input complete

--- client/components/Input.jsx
+++ client/components/Input.jsx
@@ -22,7 +22,7 @@ class Input extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          Name:
+          Angel Name:
           <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
