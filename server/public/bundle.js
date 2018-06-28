c45b176bfdde64a830b61b0c2fbf371932b6407b
Caitlyn Bayley <caitlyn.bayley@gmail.com>
2018-06-28 20:33:52 +1200

    update

--- server/public/bundle.js
+++ server/public/bundle.js
@@ -21108,19 +21108,27 @@ var _react2 = _interopRequireDefault(_react);
 
 var _reactRouterDom = __webpack_require__(46);
 
-var _Radiobuttons = __webpack_require__(77);
+var _Header = __webpack_require__(77);
+
+var _Header2 = _interopRequireDefault(_Header);
+
+var _Radiobuttons = __webpack_require__(78);
 
 var _Radiobuttons2 = _interopRequireDefault(_Radiobuttons);
 
-var _Input = __webpack_require__(78);
+var _Input = __webpack_require__(79);
 
 var _Input2 = _interopRequireDefault(_Input);
 
-var _NamesPool = __webpack_require__(79);
+var _Dropdown = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./Dropdown\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+
+var _Dropdown2 = _interopRequireDefault(_Dropdown);
+
+var _NamesPool = __webpack_require__(80);
 
 var _NamesPool2 = _interopRequireDefault(_NamesPool);
 
-var _Submit = __webpack_require__(80);
+var _Submit = __webpack_require__(81);
 
 var _Submit2 = _interopRequireDefault(_Submit);
 
@@ -21153,9 +21161,11 @@ var App = function (_React$Component) {
         _react2.default.createElement(
           'div',
           { className: 'app' },
-          _react2.default.createElement(_reactRouterDom.Route, { path: '/home', component: _NamesPool2.default }),
+          _react2.default.createElement(_reactRouterDom.Route, { path: '/home', component: _Header2.default }),
           _react2.default.createElement(_reactRouterDom.Route, { path: '/home', component: _Radiobuttons2.default }),
           _react2.default.createElement(_reactRouterDom.Route, { path: '/home', component: _Input2.default }),
+          _react2.default.createElement(_reactRouterDom.Route, { path: '/home', component: _Dropdown2.default }),
+          _react2.default.createElement(_reactRouterDom.Route, { path: '/home', component: _NamesPool2.default }),
           _react2.default.createElement(_reactRouterDom.Route, { path: '/home', component: _Submit2.default })
         )
       );
@@ -24591,6 +24601,36 @@ module.exports = hoistNonReactStatics;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.Header = undefined;
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Header = exports.Header = function Header() {
+  return _react2.default.createElement(
+    'div',
+    { className: 'header' },
+    _react2.default.createElement(
+      'h1',
+      null,
+      ' This is a header '
+    )
+  );
+};
+
+/***/ }),
+/* 78 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -24658,7 +24698,7 @@ var Radiobuttons = function (_React$Component) {
 exports.default = Radiobuttons;
 
 /***/ }),
-/* 78 */
+/* 79 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -24731,7 +24771,7 @@ var Input = function (_React$Component) {
 exports.default = Input;
 
 /***/ }),
-/* 79 */
+/* 80 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -24757,7 +24797,7 @@ var Pool = exports.Pool = function Pool() {
 };
 
 /***/ }),
-/* 80 */
+/* 81 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
