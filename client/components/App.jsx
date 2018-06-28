aa2bcbeda9cbf95930aa3d16af75eafc7c896a18
Rachael Peri <raheraperi@Rachaels-MacBook-Air.local>
2018-06-28 21:13:59 +1200

    finished dropdown component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,7 @@ import RadioButtons from './Radiobuttons'
 import Input from './Input'
 import Pool from './NamesPool'
 import Results from './Submit'
+import DropDown from './DropDown'
 
 class App extends React.Component {
   constructor (props) {
@@ -21,6 +22,8 @@ class App extends React.Component {
           <Route path='/home' component={RadioButtons} />
           <Route path='/home' component={Input} />
           <Route path='/home' component={Results} />
+          <Route path='/home' component={DropDown} />
+
         </div>
       </Router>
     )
