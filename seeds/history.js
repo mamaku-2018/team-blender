27c29b7332f2afd40f410584b81ab638c0e74a3b
Rachael Peri <raheraperi@Rachaels-MacBook-Air.local>
2018-06-29 12:10:33 +1200

    server route testing complete

--- seeds/history.js
+++ seeds/history.js
@@ -5,10 +5,10 @@ exports.seed = function (knex, Promise) {
       // Inserts seed entries
       return knex('history').insert([
         {id: 1, student_id: 1, team: 'Pegasus', week: 1},
-        {id: 1, student_id: 2, team: 'Dragon', week: 1},
-        {id: 1, student_id: 3, team: 'Pegasus', week: 1},
-        {id: 1, student_id: 4, team: 'Dragon', week: 1},
-        {id: 1, student_id: 5, team: 'Pegasus', week: 1}
+        {id: 2, student_id: 2, team: 'Dragon', week: 2},
+        {id: 3, student_id: 3, team: 'Pegasus', week: 3},
+        {id: 4, student_id: 4, team: 'Dragon', week: 4},
+        {id: 5, student_id: 5, team: 'Pegasus', week: 5}
 
       ])
     })
--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const path = require('path')
 const express = require('express')
 
-// const fruitRoutes = require('./routes/mainRoutes')
+const mainRoutes = require('./routes/mainRoutes')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-// server.use('/api/v1/peeps', mainRoutes)
+server.use('/v1/students', mainRoutes)
 
 module.exports = server
